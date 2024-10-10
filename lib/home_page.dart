2befce8d23e38cc1753dfeeeb93e44cdee5d4a3e
Hercules <hmls235@gmail.com>
2024-10-09 21:37:01 -0300

    quarto commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,16 +4,27 @@ import 'package:TaskFlow/assets/fonts/app_fonts.dart';
 import 'package:TaskFlow/assets/colors/app_colors.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   static String tag = 'home_page';
 
   const HomePage({super.key});
 
   @override
+  State<StatefulWidget> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-        systemNavigationBarColor: AppColors.primaryGreenColor,
-        systemNavigationBarIconBrightness: Brightness.light));
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        systemNavigationBarColor: AppColors.primaryWhiteColor,
+        systemNavigationBarIconBrightness: Brightness.dark,
+      ),
+    );
+
     const transition = Hero(
       tag: 'home',
       child: Padding(
@@ -25,15 +36,104 @@ class HomePage extends StatelessWidget {
     final body = Container(
       width: MediaQuery.of(context).size.width,
       padding: const EdgeInsets.only(left: 20.0, top: 30.0, right: 20.0),
-      child: Directionality(
-          textDirection: TextDirection.ltr,
-          child: Text(
-            'Listas',
-            style: TextStyle(
-                fontFamily: AppFonts.montserrat,
-                fontWeight: FontWeight.w500,
-                fontSize: 24.0),
-          )),
+      child: Column(
+        textDirection: TextDirection.ltr,
+        children: <Widget>[
+          Row(
+            textDirection: TextDirection.ltr,
+            children: <Widget>[
+              Expanded(
+                child: Text(
+                  'Listas',
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                      fontFamily: AppFonts.montserrat,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 24.0),
+                ),
+              )
+            ],
+          ),
+          Spacer(),
+          Row(
+            textDirection: TextDirection.ltr,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(bottom: 20.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16.0),
+                  boxShadow: [
+                    BoxShadow(
+                        color: AppColors.secondaryBlackColor,
+                        offset: Offset(0, 4),
+                        blurRadius: 20.0,
+                        spreadRadius: -16),
+                  ],
+                ),
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    print('clicado');
+                  },
+                  label: Text(
+                    'Nova Lista',
+                    style: TextStyle(
+                      color: AppColors.primaryWhiteColor,
+                    ),
+                  ),
+                  icon: Icon(Icons.add),
+                  style: ButtonStyle(
+                    iconColor:
+                        WidgetStatePropertyAll(AppColors.primaryWhiteColor),
+                    backgroundColor:
+                        WidgetStatePropertyAll(AppColors.primaryGreenColor),
+                    padding: WidgetStatePropertyAll(EdgeInsets.all(16.0)),
+                    shape: WidgetStatePropertyAll(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(16.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: AppColors.secondaryBlackColor,
+                      offset: Offset(2, 4),
+                      blurRadius: 4.0,
+                    ),
+                  ],
+                ),
+                child: ElevatedButton(
+                  onPressed: () {
+                    print('Cliquei');
+                  },
+                  style: ButtonStyle(
+                    backgroundColor:
+                        WidgetStatePropertyAll(AppColors.primaryGreenColor),
+                    padding: WidgetStatePropertyAll(EdgeInsets.all(16.0)),
+                    shape: WidgetStatePropertyAll(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(16.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  child: Icon(
+                    Icons.search,
+                    color: AppColors.primaryWhiteColor,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
 
     return Scaffold(
@@ -46,7 +146,7 @@ class HomePage extends StatelessWidget {
               onPressed: () {
                 Scaffold.of(context).openDrawer();
               },
-              icon: Icon(
+              icon: const Icon(
                 Icons.menu,
                 size: 30,
                 color: AppColors.primaryWhiteColor,
@@ -61,7 +161,7 @@ class HomePage extends StatelessWidget {
           height: 54,
         ),
         actions: [
-          Padding(
+          const Padding(
             padding: EdgeInsets.only(right: 12.0),
             child: CircleAvatar(
               radius: 16.0,
--- lib/repositories/lists_repository.dart
+++ lib/repositories/lists_repository.dart
@@ -1,7 +1,7 @@
 import 'package:TaskFlow/model/list.dart';
 
-class ListsRepository {
-  static List<Lists> table = [
-    Lists(title: title, actualData: actualData, reminder: reminder)
-  ];
-}
+// class ListsRepository {
+//   static List<Lists> table = [
+//     Lists(title: title, actualData: actualData, reminder: reminder)
+//   ];
+// }
