1349f3b1d52a7fc0ac383c581eb3cf68469dcc3b
Hercules Matheus Lima Silva <hercules.silva@parnamirim.rn>
2024-11-07 14:09:37 -0300

    fix bug from login page

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:taskflow/assets/fonts/app_fonts.dart';
+import 'package:taskflow/assets/colors/app_colors.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -14,6 +16,10 @@ class _LoginPageState extends State<LoginPage> {
   final formKey = GlobalKey<FormState>();
   final email = TextEditingController();
   final password = TextEditingController();
+  final FocusNode _passwordFocusNode = FocusNode();
+  final FocusNode _emailFocusNode = FocusNode();
+  bool _isPasswordFocused = false;
+  bool _isEmailFocused = false;
 
   bool isLogin = true;
   late String title;
@@ -24,6 +30,16 @@ class _LoginPageState extends State<LoginPage> {
   void initState() {
     super.initState();
     setFormAction(true);
+    _passwordFocusNode.addListener(() {
+      setState(() {
+        _isPasswordFocused = _passwordFocusNode.hasFocus;
+      });
+    });
+    _emailFocusNode.addListener(() {
+      setState(() {
+        _isEmailFocused = _emailFocusNode.hasFocus;
+      });
+    });
   }
 
   setFormAction(bool action) {
@@ -44,54 +60,101 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.primaryGreenColor,
+      ),
+      backgroundColor: AppColors.primaryWhiteColor,
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.only(top: 100),
+          padding: EdgeInsets.only(top: 30),
           child: Form(
             key: formKey,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text(
-                  title,
-                  style: TextStyle(
-                    fontFamily: AppFonts.montserrat,
-                    fontWeight: FontWeight.w500,
-                    letterSpacing: -1.5,
-                  ),
-                ),
                 Padding(
-                  padding: EdgeInsets.all(24),
-                  child: TextFormField(
-                    controller: email,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(), labelText: 'Email'),
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Informe o email corretamente';
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(24),
-                  child: TextFormField(
-                    controller: password,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Senha',
+                  padding: EdgeInsets.only(right: 200),
+                  child: Text(
+                    title,
+                    style: TextStyle(
+                      fontFamily: AppFonts.montserrat,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20.0,
+                      letterSpacing: -1.5,
                     ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Informe sua senha!';
-                      } else if (value.length < 6) {
-                        return 'Sua senha deve ter no mínimo 6 caracteres';
-                      }
-                      return null;
-                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20.0),
+                  child: CircleAvatar(
+                    radius: 16.0,
+                    backgroundImage:
+                        AssetImage('lib/assets/images/generic-avatar.png'),
+                    backgroundColor: Colors.transparent,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(24),
+                  child: SizedBox(
+                    width: 210,
+                    child: TextFormField(
+                      controller: email,
+                      focusNode: _emailFocusNode,
+                      cursorColor: AppColors.secondaryGreenColor,
+                      decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: AppColors.secondaryGreenColor,
+                          ),
+                        ),
+                        labelText: 'Email',
+                        labelStyle: TextStyle(
+                          color: _isEmailFocused
+                              ? AppColors.secondaryGreenColor
+                              : AppColors.primaryBlackColor,
+                        ),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Informe o email corretamente';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(24),
+                  child: SizedBox(
+                    width: 210,
+                    child: TextFormField(
+                      controller: password,
+                      focusNode: _passwordFocusNode,
+                      cursorColor: AppColors.secondaryGreenColor,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: AppColors.secondaryGreenColor,
+                          ),
+                        ),
+                        labelText: 'Senha',
+                        labelStyle: TextStyle(
+                          color: _isPasswordFocused
+                              ? AppColors.secondaryGreenColor
+                              : AppColors.primaryBlackColor,
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Informe sua senha!';
+                        } else if (value.length < 6) {
+                          return 'Sua senha deve ter no mínimo 6 caracteres';
+                        }
+                        return null;
+                      },
+                    ),
                   ),
                 ),
               ],
